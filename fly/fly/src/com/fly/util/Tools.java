0863e1daa3926b2262bcf8c4d0cf8f43a71d7803
joegana <joegana@126.com>
2014-12-27 21:53:19 +0800

    wheel view

--- fly/fly/src/com/fly/util/Tools.java
+++ fly/fly/src/com/fly/util/Tools.java
@@ -1,7 +1,11 @@
 package com.fly.util;
 
+import java.io.Closeable;
+import java.io.IOException;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
+
+import android.view.View;
 
 import com.fly.sdk.util.TextUtils;
 
@@ -27,5 +31,62 @@ public class Tools {
 
 		return m.matches();
 	}
+	
+	 public static void closeIO(Closeable... closeables) {
+	        if (null == closeables || closeables.length <= 0) {
+	            return;
+	        }
+	        for (Closeable cb : closeables) {
+	            try {
+	                if (null == cb) {
+	                    continue;
+	                }
+	                cb.close();
+	            } catch (IOException e) {
+	                Debug.log.e("closeIO", "close IO ERROR...", e);
+	            }
+	        }
+	    }
+	  /**
+	     * 获取控件的高度，如果获取的高度为0，则重新计算尺寸后再返回高度
+	     * @param view
+	     * @return
+	     */
+	    public static int getViewMeasuredHeight(View view){
+//	        int height = view.getMeasuredHeight();
+//	        if(0 < height){
+//	            return height;
+//	        }
+	        calcViewMeasure(view);
+	        return view.getMeasuredHeight();
+	    }
+
+	    /**
+	     * 获取控件的宽度，如果获取的宽度为0，则重新计算尺寸后再返回宽度
+	     * @param view
+	     * @return
+	     */
+	    public static int getViewMeasuredWidth(View view){
+//	        int width = view.getMeasuredWidth();
+//	        if(0 < width){
+//	            return width;
+//	        }
+	        calcViewMeasure(view);
+	        return view.getMeasuredWidth();
+	    }
+	    
+	  /**
+	     * 测量控件的尺寸
+	     * @param view
+	     */
+	    public static void calcViewMeasure(View view){
+//	        int width = View.MeasureSpec.makeMeasureSpec(0,View.MeasureSpec.UNSPECIFIED);
+//	        int height = View.MeasureSpec.makeMeasureSpec(0,View.MeasureSpec.UNSPECIFIED);
+//	        view.measure(width,height);
+
+	        int width = View.MeasureSpec.makeMeasureSpec(0,View.MeasureSpec.UNSPECIFIED);
+	        int expandSpec = View.MeasureSpec.makeMeasureSpec(Integer.MAX_VALUE >> 2, View.MeasureSpec.AT_MOST);
+	        view.measure(width, expandSpec);
+	    }
 	
 }
